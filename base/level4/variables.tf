86567c37845f7fe60938b3b8c4199b55b71c8fa3
Shaun Clarke <shaunclarke43@gmail.com>
2022-08-19 09:36:07 -0700

    updating blue green deployment

---
+++ base/level4/variables.tf
@@ -0,0 +1,16 @@
+# instance type for ec2
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     = "t2.small"
+}
+
+# key to connect to ec2.
+variable "key_name" {
+  description = "key to connect to main-ec2 resource"
+  default     = "main"
+}
+
+# variable for environment name
+variable "env" {
+  description = "input variable for environment name"
+}
